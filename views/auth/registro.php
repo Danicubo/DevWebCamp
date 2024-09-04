35f2b0f86718d63687362dba7eea7e0a119ec64b
Daniel Cuevas Botero <danycubo2002@hotmail.com>
2024-09-04 17:49:46 -0500

    new commit

--- views/auth/registro.php
+++ views/auth/registro.php
@@ -2,6 +2,8 @@
     <h2 class="auth__heading"><?php echo $titulo; ?></h2>
     <p class="auth__texto">Registrate en DevWebCamp</p>
 
+    <?php require_once __DIR__ . '/../templates/alertas.php' ?>
+
     <form action="/registro" method="POST" class="formulario">
     <div class="formulario__campo">
         <label for="nombre" class="formulario__label">Nombre</label>
@@ -11,6 +13,7 @@
         placeholder="Tu nombre"
         id="nombre"
         name="nombre"
+        value="<?php echo $usuario->nombre; ?>"
         />
     </div>
     <div class="formulario__campo">
@@ -21,6 +24,18 @@
         placeholder="Tu apellido"
         id="apellido"
         name="apellido"
+        value="<?php echo $usuario->apellido; ?>"
+        />
+    </div>
+    <div class="formulario__campo">
+        <label for="email" class="formulario__label">Email</label>
+        <input 
+        type="email"
+        class="formulario__input"
+        placeholder="Tu email"
+        id="email"
+        name="email"
+        value="<?php echo $usuario->email; ?>"
         />
     </div>
     <div class="formulario__campo">
@@ -36,7 +51,7 @@
     <div class="formulario__campo">
         <label for="password2" class="formulario__label">Repetir Password</label>
         <input 
-        type="password2"
+        type="password"
         class="formulario__input"
         placeholder="Repite tu passowrd"
         id="password2"
